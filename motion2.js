9754fd3b3a969d1efc0627835894beea7c9d6381
Erhor1 <30455370+Erhor1@users.noreply.github.com>
2018-04-08 01:38:04 -0700

    Added Inline comments/Fixed LED illumination time and added testdata.js

--- motion2.js
+++ motion2.js
@@ -1,16 +1,16 @@
-//====================================================GOOGLE FIREBASE INITIALIZATION======================================================================
+//====================================================GOOGLE FIREBASE INITIALIZATION (FROM WEEK 3 LAB)===========================================================
 var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fit3140t15p1.firebaseio.com/" 
+  databaseURL: "https://fit3140t15p1.firebaseio.com/"
 });
 
 var db = admin.database();
 var ref = db.ref("/motionSensorData"); // channel name
 
-//=============================================================SOCKETIO INITIALIZATION===========================================================================
+//=============================================================SOCKETIO INITIALIZATION (FROM WEEK 2 LAB)==========================================================
 var fs =require('fs')
          , http=require('http')
          , socket=require('socket.io');
@@ -32,15 +32,15 @@ var board = new five.Board();
 var starttime=0;
 var endtime=0;
 var ledstate=0;
-var lednewperson=0;
+var lednewperson=0; //New person flag
 var idarray=[];
 var timearray=[];
 var newdata=0;
 var ledcounter=0;
 
 //Thresholds
-var offset=1000*3;
-var threshold=3;
+var offset=1000*3; //Offset when physical motion ends and Arduino motion ends
+var threshold=3; //Threshold between long and short motions
 
 //LED/Sensor Enable
 var ledon=1;
@@ -53,11 +53,11 @@ board.on("ready", function() {
 
   motion.on("calibrated", function() {
       console.log("SENSOR IS WORKING");
-      newdata=1;
+      newdata=1; //To differentiate between old data already on the database so LED does not light up
   });
 
   motion.on("motionstart", function() {
-      if (sensoron){
+      if (sensoron){ //Sensor toggle using socketio
           starttime=new Date().getTime();
           console.log("Motion Start at " + starttime);
       }
@@ -66,32 +66,30 @@ board.on("ready", function() {
   motion.on("motionend", function() {
       if (sensoron){
           endtime=new Date().getTime();
-          var timeelapsed= (endtime-starttime-offset)/1000
+          var timeelapsed= (endtime-starttime-offset)/1000 //An offset is used to account for the time it takes for the motion pin to go low
           console.log("Motion lasted : " + timeelapsed);
-          if ((endtime-starttime-offset)>0){  
-            ref.push({
+          if ((endtime-starttime-offset)>0){  //Remove negative times
+            ref.push({ //Push to Google Firebase
                 id:timearray.length,
                 time:timeelapsed
             });
           }
       }
   });
-        
-//========================================================================SOCKETIO CODE===================================================================================
-    
+            
 ref.on("child_added", function(snapshot) {
     var newentry = snapshot.val();
     idarray.push(newentry.id);
-    timearray.push(newentry.time);
+    timearray.push(newentry.time); //Append to an array so it is easier to operate on instead of getting from database
     console.log(timearray);
-    if (timearray[timearray.length-1]>threshold && timearray[timearray.length-2]>threshold && timearray[timearray.length-3]<threshold && timearray[timearray.length-4]>threshold){
+    if (timearray[timearray.length-1]>threshold && timearray[timearray.length-2]>threshold && timearray[timearray.length-3]<threshold && timearray[timearray.length-4]>threshold){ //Check if new person detected
         if (newdata){
-            if (!ledstate){
+            if (!ledstate){ //If LED is not already on (i.e. someone new is entering the corridor), turn LED on
                 console.log('New person detected');
                 ledstate=1;
-                led.strobe(0.05,CheckLEDState);    
+                led.strobe(1,CheckLEDState); //Non-blocking way of turning LED on using callback function CheckLEDState (called every time LED is off/on)    
             }
-            else if(ledstate){
+            else if(ledstate){ //If someone is already in corridor, (LED is on) extend LED illumination by 5 seconds using lednewperson flag
                 console.log('Extending LED');
                 lednewperson=1;
             }
@@ -100,6 +98,8 @@ ref.on("child_added", function(snapshot) {
     
 });
 
+//========================================================================SOCKETIO CODE===================================================================================
+    
 io.listen(server).on('connection', function (socket) {
     console.log('User Connected')
     
@@ -135,7 +135,7 @@ io.listen(server).on('connection', function (socket) {
     });
 
     socket.on('reset', function(){
-      ref.remove()
+      ref.remove() //Clear the database
           .then(function() {
             idarray=[];
             timearray=[];
@@ -143,26 +143,27 @@ io.listen(server).on('connection', function (socket) {
       });
     });
     
-    socket.on('msg',function(msg){
+    socket.on('msg',function(msg){ //Debugging tool to display stuff in node.js console
         console.log(msg);    
     });
   });
     
-    function CheckLEDState(){
-        if (ledon){
-            if (ledstate && ledcounter>15000){
+    
+    function CheckLEDState(){ //Function to illuminate corridor
+        if (ledon){ //Check if LED is enabled
+            if (ledstate && ledcounter>9667){ //Once 15 seconds elapsed, turn off led and reset everything
                 console.log('Stopped LED');
                 ledcounter=0;
                 ledstate=0;
                 led.stop().off();
             }
             else{
-                if (lednewperson){
+                if (lednewperson){ //Check lednewperson flag every iteration to decide whether to extend illumination by 5 seconds
                     lednewperson=0;
                     console.log('On for 5 seconds')
-                    ledcounter-=5000;
+                    ledcounter-=3222; //Around 5 seconds
                 }
-                ledcounter+=1;
+                ledcounter+=1; //LED counter should increment by the same amount every time (around 1 millisecond), so we can use it to time how long the LED is on
             }   
         }
         else{
