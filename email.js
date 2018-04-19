6ef0f2db0bbec0a6c7aeac58006cd4986cf38b0f
bzrk <abngu2@student.monash.edu>
2018-04-19 16:56:07 +1000

    Create second application

---
+++ email.js
@@ -0,0 +1,25 @@
+//====================================================GOOGLE FIREBASE INITIALIZATION (FROM WEEK 3 LAB)===========================================================
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fit3140t15p1.firebaseio.com/"
+});
+
+var db = admin.database();
+var ref = db.ref("/motionSensorData"); // channel name
+
+//=============================================================SOCKETIO INITIALIZATION (FROM WEEK 2 LAB)==========================================================
+var fs =require('fs')
+         , http=require('http')
+         , socket=require('socket.io');
+
+var server=http.createServer(function(req, res) {
+            res.writeHead(200, { 'Content-type': 'text/html'});
+            res.end(fs.readFileSync(__dirname+'/index.html'));
+            }).listen(8080, function() {
+            console.log('Listening at: http://localhost:8080');
+ });
+
+ var io = socket(server);
