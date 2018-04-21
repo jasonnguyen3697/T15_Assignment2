d905473feb6dfa77efca4fee59e862b4c974a92a
Ming-Yiu <37524280+Ming-Yiu@users.noreply.github.com>
2018-04-21 14:52:26 +1000

    Update email.js

--- email.js
+++ email.js
@@ -25,28 +25,61 @@ var nodemailer = require('nodemailer');
 var io = socket(server);
 var t1;
 var t2;
+var starttimearray=[];
+var endtimearray=[];
+var timestamparray=[];
+  
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'fit3140.team15.s12018@gmail.com',
     pass: 'fit3140t15'
+  },
+  tlc:{
+    rejectUnauthorized:false
   }
 });
 var mailOptions;
-
-
+var fivemail='These are the last five motions:'
 
 ref.on("child_added", function(snapshot) {
     var newentry = snapshot.val();
+
     idarray.push(newentry.id);
+    timestamparray.push(newentry.time_stamp);
     starttimearray.push(newentry.start_time); //Append to an array so it is easier to operate on instead of getting from database
     endtimearray.push(newentry.end_time);
+    if (timestamparray.length<5) {
+      for (i=0;i<timestamparray.length;i++){
+        fivemail+='\nMotion ';
+        fivemail+=i;
+        fivemail+=': Timestamp= ';
+        fivemail+=timestamparray[i]';
+        fivemail+=', start time= ';
+        fivemail+=starttimearray[i]';
+        fivemail+=', end time= ';
+        fivemail+=endtimearray[i]';
+      }
+    }
+    else
+    {
+        for (i=0;i<5;i++){
+        fivemail+='\nMotion ';
+        fivemail+=i;
+        fivemail+=': Timestamp= ';
+        fivemail+=timestamparray[timestamparray.length-6+i]';
+        fivemail+=', start time= ';
+        fivemail+=starttimearray[starttimearray.length-6+i]';
+        fivemail+=', end time= ';
+        fivemail+=endtimearray[endtimearray.length-6+i]';
+      }
+    }  
     console.log(timearray);
     mailOptions = {
       from: 'fit3140.team15.s12018@gmail.com',
-      to: 'myfriend@yahoo.com',
+      to: 'fit3140.team15.s12018@gmail.com',
       subject: 'Last 5 motions',
-      text: 'These are the last five motions: '
+      text: 
     };
       transporter.sendMail(mailOptions, function(error, info){
       if (error) {
@@ -58,7 +91,7 @@ ref.on("child_added", function(snapshot) {
       from: 'fit3140.team15.s12018@gmail.com',
       to: 'myfriend@yahoo.com',
       subject: 'Motion in threshold',
-      text: 'The last motion was between {{t1}} and {{t2}}'
+      text: 'The last motion was between ' + t1 +' and ' +t2
     };
     if (endtimearray[endtimearray.length-1]-starttimearray[starttimearray.length-1]>t1 && endtimearray[endtimearray.length-1]-starttimearray[starttimearray.length-1]<t2)
     {
